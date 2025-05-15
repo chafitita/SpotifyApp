427a78449264f413d46c7a8459d26198eee5a44f
levrino <mlevrino@facultad.sanfrancisco.utn.edu.ar>
2025-05-14 21:34:54 -0300

    minor changes v4

--- Spotify-Backend/server.js
+++ Spotify-Backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: ['http://127.0.0.1:5173', 'https://shyntify.netlify.app'],
+  origin: process.env.FRONTEND_URL || 'http://127.0.0.1:5173',
   credentials: true,
   optionsSuccessStatus: 200
 };
